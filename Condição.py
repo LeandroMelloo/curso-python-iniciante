435df64917e76259725ca2bdcd10c2d5d413857b
Leandro Paulino de Mello <leandropaulino@brq.com>
2020-05-24 20:58:24 -0300

    Curso python iniciante

---
+++ Condição.py
@@ -0,0 +1,10 @@
+﻿a = int(input("Primeiro valor: "))
+b = int(input("Segundo valor: "))
+if a > b:
+        print ("O primeiro número é o maior!")
+
+
+
+
+
+
---
+++ file.py
@@ -0,0 +1,56 @@
+Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 17:26:49) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import os
+
+DATADIR = ""
+DATAFILE = "beatles-diskography.csv"
+
+
+def parse_file(datafile):
+    data = []
+    with open(datafile, "r") as f:
+        for line in f:
+            print line
+
+    return data
+
+
+def test():
+    # a simple test of your implemetation
+    datafile = os.path.join(DATADIR, DATAFILE)
+    d = parse_file(datafile)
+    firstline = {'Title': 'Please Please Me', 'UK Chart Position': '1', 'Label': 'Parlophone(UK)', 'Released': '22 March 1963', 'US Chart Position': '-', 'RIAA Certification': 'Platinum', 'BPI Certification': 'Gold'}
+    tenthline = {'Title': '', 'UK Chart Position': '1', 'Label': 'Parlophone(UK)', 'Released': '10 July 1964', 'US Chart Position': '-', 'RIAA Certification': '', 'BPI Certification': 'Gold'}
+
+    assert d[0] == firstline
+    assert d[9] == tenthline
+
+    
+test()
+SyntaxError: multiple statements found while compiling a single statement
+>>> import os
+>>> DATADIR = ""
+DATAFILE = "beatles-diskography.csv"
+
+
+def parse_file(datafile):
+    data = []
+    with open(datafile, "r") as f:
+        for line in f:
+            print line
+
+    return data
+
+
+def test():
+    # a simple test of your implemetation
+    datafile = os.path.join(DATADIR, DATAFILE)
+    d = parse_file(datafile)
+    firstline = {'Title': 'Please Please Me', 'UK Chart Position': '1', 'Label': 'Parlophone(UK)', 'Released': '22 March 1963', 'US Chart Position': '-', 'RIAA Certification': 'Platinum', 'BPI Certification': 'Gold'}
+    tenthline = {'Title': '', 'UK Chart Position': '1', 'Label': 'Parlophone(UK)', 'Released': '10 July 1964', 'US Chart Position': '-', 'RIAA Certification': '', 'BPI Certification': 'Gold'}
+
+    assert d[0] == firstline
+    assert d[9] == tenthline
+
+    
+test()
